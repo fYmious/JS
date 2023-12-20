14a79638e74520b2d33a922f026cf80b5c33b546
dolbik_2 <132186293+fYmious@users.noreply.github.com>
2023-12-20 16:36:54 +0500

    msg

--- 001codewars/main.js
+++ 001codewars/main.js
@@ -1,15 +1,6 @@
-function digPow(n, p){
-    let sum = 0
-    for (const digit of n.toString()) {
-        sum += parseInt(digit) ** p
-        p++
-    }
-    if (sum % n === 0){
-        return sum / n
-    } else {
-        return -1
-    }
+function cakes(recipe, available) {
+
 }
 
 
-console.log(digPow(46288, 3))
+
